99e453fe3905e8ffdbb97872719fc6472d0b2164
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-04-15 15:26:06 +0300

    Get list of stages

--- WebAPI/Controllers/GeneralController.cs
+++ WebAPI/Controllers/GeneralController.cs
@@ -1,16 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Data;
 
 namespace WebAPI.Controllers
 {
-    public class GeneralController : Controller
+    [ApiController]
+    public class GeneralController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public GeneralController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet("/api/stages")]
+        public async Task<IActionResult> stagesIndex()
+        {
+            var stages = await _context.Stages.ToListAsync();
+            if (stages == null) return NotFound();
+
+            return Ok(stages);
         }
     }
 }
